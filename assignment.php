cc27993800f684ff69385e452d204991d096cf71
Aryan99425 <101926061+Aryan99425@users.noreply.github.com>
2022-05-03 20:46:15 +0530

    Add files via upload

---
+++ assignment.php
@@ -0,0 +1,21 @@
+<html>
+<body bgcolor="skyblue">
+<?php
+$a=10;
+echo"<center>ARYAN THAKUR<br><hr>";
+echo"ASSIGNMEMT OPERATORS<br><hr>";
+$a=20;
+echo"$a<br>";
+$a+=56;
+echo"$a<br>";
+$a-=40;
+echo"$a<br>";
+$a*=20;
+echo"$a<br>";
+$a/=30;
+echo"$a<br>";
+$a%=21;
+echo"$a<br>";
+?>
+</body>
+</html>
